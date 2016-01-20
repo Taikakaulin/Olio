9376882d5d701b8c430d927c780db25afaf94c48
Taikakaulin <paukokkonen96@gmail.com>
2016-01-20 12:44:22 +0200

    k

---
+++ github/Demo04/CarApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Speed = 300;
+            Console.WriteLine("Cars speed is " + car.Speed);
+            Console.ReadLine();
+        }
+    }
+}
