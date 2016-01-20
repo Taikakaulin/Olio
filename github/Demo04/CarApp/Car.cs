d9f69e1f7195acf64945dde1f600f3627296eab3
Taikakaulin <paukokkonen96@gmail.com>
2016-01-20 13:12:09 +0200

    k

--- github/Demo04/CarApp/Car.cs
+++ github/Demo04/CarApp/Car.cs
@@ -8,7 +8,28 @@ namespace CarApp
 {
     class Car
     {
-        public int Speed; 
+        //MAX speed
+        private readonly int maxSpeed = 200;
+        //member field
+        private int speed; 
+        //property
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value <= maxSpeed) speed = value;
+                else
+                {
+                    Console.WriteLine("Too much speed + set to maximum!");
+                    speed = maxSpeed;
+                }
+            }
+        }
+       
     }
    
 }
